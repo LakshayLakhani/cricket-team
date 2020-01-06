187362edf9b7c8e6cf45e49d870533dcb61d4fbe
Lakshay <4lakshaylakhani@gmail.com>
2020-01-07 01:17:42 +0530

    database table for matches and points

--- cricket_team/match/models.py
+++ cricket_team/match/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from shared.models import BaseTimestampModel
+from team.models import Team
+
+class Match(BaseTimestampModel):
+    team_1 = models.ForeignKey(
+        Team, on_delete=models.CASCADE, null=True, blank=True, related_name="team1")
+    team_2 = models.ForeignKey(
+        Team, on_delete=models.CASCADE, null=True, blank=True, related_name="team2")
+    winner = models.ForeignKey(
+        Team, on_delete=models.CASCADE, null=True, blank=True, related_name="winner")
+
+    def __str__(self):
+        return str(self.winner.name)
+
+
+class Points(BaseTimestampModel):
+	points = models.IntegerField()
+	team = models.OneToOneField(
+        Team, on_delete=models.CASCADE, null=True, blank=True)
