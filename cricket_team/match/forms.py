0d0f33d4878e89dd137720de1c982e34be6dbb10
Lakshay <4lakshaylakhani@gmail.com>
2020-01-08 02:09:43 +0530

    some migrations changes

--- cricket_team/match/forms.py
+++ cricket_team/match/forms.py
@@ -2,20 +2,19 @@ from django import forms
 
 from match.models import Match
 
+
 class AddMatchForm(forms.ModelForm):
-    team_1_score = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'required':'required'}))
-    team_2_score = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'required':'required'}))
+    team_1_score = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'required':'required'}))
+    team_2_score = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'required':'required'}))
 
     class Meta:
         model = Match
-        fields = ('team_1', 'team_2')
+        fields = ('team_1', 'team_2',)
 
     def __init__(self, *args, **kwargs):
         super(AddMatchForm, self).__init__(*args, **kwargs)
         self.fields['team_1'].widget.attrs.update({'class': 'form-control', 'required':True})
         self.fields['team_2'].widget.attrs.update({'class': 'form-control', 'required':True})
-        # self.fields['first_name'].widget.attrs.update({'placeholder': 'First Name', 'required':True})
-        # self.fields['last_name'].widget.attrs.update({'placeholder': 'Last Name', 'required':True})
 
     def clean_team_2(self):
         cleaned_data = super(AddMatchForm, self).clean()
@@ -23,6 +22,6 @@ class AddMatchForm(forms.ModelForm):
         team_2 = cleaned_data.get("team_2")
 
         if team_1 == team_2:
-        	raise forms.ValidationError("match between 2 same team not possible.")
+        	raise forms.ValidationError("Match between 2 same team not possible.")
 
         return self.cleaned_data['team_2']
--- cricket_team/match/views.py
+++ cricket_team/match/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
 
 from match.forms import AddMatchForm
+from match.models import Points
 # Create your views here.
 
 def add_match(request):
     form = AddMatchForm(request.POST or None)
     if request.method == 'POST':
-    	if form.is_valid():
+        if form.is_valid():
+            team1_score = form.cleaned_data["team_1_score"]
+            team2_score = form.cleaned_data["team_2_score"]
             obj = form.save()
-            print("obj ++++++++++++++++++++++++++++++++++")
-            print(obj)
-            company_and_user_obj = UsersWithRolesInCompany.objects.get_or_create(user=user)
-            current_site = get_current_site(request)
-            email_subject = 'Activate Your Account'
-            message = render_to_string('registration/activate_account.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(email_subject, message, to=[to_email])
-            email.send()
+
+            if team1_score > team2_score:
+                obj.winner = obj.team_1
+            else:
+                obj.winner = obj.team_2
+            obj.save()
+
+            score = Points()
+            score.match = obj
+            score.team1 = team1_score
+            score.team2 = team2_score
+            score.save()
+
             messages.add_message(request, messages.SUCCESS, "We have sent you an email, please confirm your email address to complete registration!.")
             return redirect("login")
     else:
